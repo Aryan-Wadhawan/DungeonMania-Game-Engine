ce0613ee9a17bf4880bd3be6d4bcd66a87c3729a
Aryan Wadhawan <a.wadhawan@student.unsw.edu.au>
2025-04-17 21:02:00 +1000

    fixing lint, Graph Node switch casw statement longer than 120 lines

--- app/src/main/java/dungeonmania/map/GraphNodeFactory.java
+++ app/src/main/java/dungeonmania/map/GraphNodeFactory.java
@@ -13,11 +13,36 @@ public class GraphNodeFactory {
 
     private static GraphNode constructEntity(JSONObject jsonEntity, EntityFactory factory) {
         String type = jsonEntity.getString("type");
-        return switch (type) {
-        case "player", "zombie_toast", "zombie_toast_spawner", "mercenary", "wall", "boulder", "switch", "exit", "treasure", "wood", "arrow", "bomb", "invisibility_potion", "invincibility_potion", "portal", "sword", "spider", "wire", "light_bulb_off", "light_bulb_on", "switch_door", "switch_door_open", "door", "key" -> new GraphNode(
-                factory.createEntity(jsonEntity));
-        default -> throw new IllegalArgumentException(
-                String.format("Failed to recognise '%s' entity in GraphNodeFactory", type));
-        };
+
+        switch (type) {
+        case "player":
+        case "zombie_toast":
+        case "zombie_toast_spawner":
+        case "mercenary":
+        case "wall":
+        case "boulder":
+        case "switch":
+        case "exit":
+        case "treasure":
+        case "wood":
+        case "arrow":
+        case "bomb":
+        case "invisibility_potion":
+        case "invincibility_potion":
+        case "portal":
+        case "sword":
+        case "spider":
+        case "wire":
+        case "light_bulb_off":
+        case "light_bulb_on":
+        case "switch_door":
+        case "switch_door_open":
+        case "door":
+        case "key":
+            return new GraphNode(factory.createEntity(jsonEntity));
+        default:
+            throw new IllegalArgumentException(
+                    String.format("Failed to recognise '%s' entity in GraphNodeFactory", type));
+        }
     }
 }
